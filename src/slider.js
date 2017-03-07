d50dc6b5736be300eb6c942ea313f5eb3cdf291b
Kesus Kim <kesuskim@gmail.com>
2017-03-07 17:38:38 +0900

    Add options, import statement

--- src/slider.js
+++ src/slider.js
@@ -1,14 +1,22 @@
 import React, { Component } from 'react';
+import '../public/lib/jssor.slider.min.js';
 
 declare var $JssorSlider$;
 declare var $JssorEasing$;
 declare var $JssorSlideshowFormations$;
 declare var $JssorSlideshowRunner$;
+declare var $JssorBulletNavigator$;
+declare var $JssorArrowNavigator$;
+declare var $JssorThumbnailNavigator$;
+declare var $JssorCaptionSlideo$;
+
 
 class Slider extends Component {
   componentDidMount() {
     (function (containerId) {
-
+        /*
+          Transition Reference: http://www.jssor.com/development/tool-slideshow-transition-viewer.html
+        */
         var _SlideshowTransitions = [{
             $Duration: 600,
             $Delay: 50,
@@ -21,7 +29,7 @@ class Slider extends Component {
             $ChessMode: { $Column: 3, $Row: 12 },
             $Easing: { $Left: $JssorEasing$.$EaseInCubic, $Top: $JssorEasing$.$EaseInCubic, $Opacity: $JssorEasing$.$EaseOutQuad },
             $Opacity: 2
-        }];
+        }, {$Duration:600,x:1,$Delay:50,$Cols:8,$Rows:4,$Easing:{$Left:$JssorEasing$.$EaseInCubic,$Opacity:$JssorEasing$.$EaseOutQuad},$Opacity:2}];
 
         var options = {
             $DragOrientation: 3,
@@ -59,8 +67,8 @@ class Slider extends Component {
             width: '600px',
             height: '300px'
           }}>
-              <div><img data-u="image" alt='ASD' data-src2="https://tryolabs.com/images/blog/post-images/2015-04-21-drag-and-drop-in-react/screenshot1.png" /></div>
-              <div><img data-u="image" alt='ASD' src="https://svbtleusercontent.com/rpdhl3zfb92hha_small.gif" /></div>
+              <div><img data-u="image" alt='ASD' data-src2="https://c1.staticflickr.com/4/3826/9721093875_1075126126_z.jpg" /></div>
+              <div><img data-u="image" alt='ASD' src="http://imgsv.imaging.nikon.com/lineup/lens/zoom/normalzoom/af-s_dx_18-140mmf_35-56g_ed_vr/img/sample/sample1_l.jpg" /></div>
           </div>
       </div>
     )
